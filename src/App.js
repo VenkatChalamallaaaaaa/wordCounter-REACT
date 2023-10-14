ce3afd9fe079ed6ad411fb6bf74a28d9bb9caab4
Venkat Chalamalla <venkat.chalamallak@gmail.com>
2023-10-14 10:12:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    const words = text.split(' ');
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Responsive Paragraph Word <br/> Count</h2>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+        </div>
+      </div>
     </div>
   );
 }
